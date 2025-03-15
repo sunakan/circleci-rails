e6e91a23736154d7a3d1ae346ca9e8c725a0ac51
sunakan <sunakan03@gmail.com>
2025-03-15 11:39:52 +0900

    rspec: hello_specを用意

---
+++ spec/sandbox/hello_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Hello World' do
+  it 'returns a greeting message' do
+    greeting = 'Hello, World!'
+    expect(greeting).to eq('Hello, World!')
+  end
+
+  it 'concatenates strings' do
+    name = 'Alice'
+    greeting = "Hello, #{name}!"
+    expect(greeting).to eq('Hello, Alice!')
+  end
+end
