96547c70c2587d0efbfc1a0eff84c0df7be892ba
sunakan <sunakan03@gmail.com>
2025-03-25 19:12:26 +0900

    rubocop対応: テーブルスキーマコメントを記述

--- db/migrate/20250325093254_create_users.rb
+++ db/migrate/20250325093254_create_users.rb
@@ -2,8 +2,8 @@
 
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table(:users) do |t|
-      t.string(:name)
+    create_table(:users, comment: 'ユーザーテーブル') do |t|
+      t.string(:name, comment: '名前')
 
       t.timestamps
     end
